90f895a59eae052c0cd9217081fa887ee3aedfc8
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-16 16:46:35 -0300

    feat: sintaxe error message

--- src/utils.c
+++ src/utils.c
@@ -7,9 +7,19 @@
 
 void lexialError(unsigned int line, const char character)
 {
-    fprintf(stderr, "\n\033[31mErro: Não esperava o caracter \"%c\"\033[m.\n\n", character);
+    fprintf(stderr, "\n\033[31mErro: Não esperava o caracter \"%c\".\033[m\n\n", character);
     fprintf(stderr, "\t%d | %s\n", line, getBufferSlice());
     fprintf(stderr, "\t%*s^-- Aqui.\n\n", getBufferPosition() + 3, "");
+
+    exit(EXIT_FAILURE);
+}
+
+
+void syntaxeError(unsigned int line, const char * world)
+{
+    fprintf(stderr, "\n\033[31mSintaxe Inválida: Não esperava por \"%s\".\033[m\n\n", world);
+    fprintf(stderr, "\t%d | %s\n", line, getBufferSlice());
+    fprintf(stderr, "\t%*s\n\n", getBufferPosition(), "^");
 
     exit(EXIT_FAILURE);
 }
--- src/utils.h
+++ src/utils.h
@@ -7,6 +7,7 @@
 
 
 void lexialError(unsigned int line, const char character);
+void syntaxeError(unsigned int line, const char * world);
 void exitOnError(const char * message, ...);
 
 #endif
