1dee656667669c553d011bd0ebc9c530b26dee69
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-17 23:12:24 -0300

    refact: stract AST to globals file

--- src/parser.h
+++ src/parser.h
@@ -4,29 +4,6 @@
 #include "utils.h"
 #include "globals.h"
 
-#define MAX_CHILDREN   3
-
-typedef enum { StmtK, ExpK } NodeKind;
-typedef enum { OpK, ConstK, IdK } ExpKind;
-typedef enum { IfK, ForK, FuncK, AssignK, WriteK } StmtKind;
-
-typedef enum { Void, Int, Bool } ExpType;
-
-typedef struct TreeNode {
-    struct TreeNode * childs;
-    struct TreeNode * next;
-
-    NodeKind nodekind;
-    union { StmtKind stmt; ExpType exp; } kind;
-    union {
-        TokenType op;
-        int val;
-        char * name;
-    } attrs;
-
-    ExpType type;
-} TreeNode;
-
 
 TreeNode * getProgramAST();
 
