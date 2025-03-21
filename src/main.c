5c878b564709a3aafdeed715b3b46dcf495d1c0b
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-20 23:55:09 -0300

    refact: get the AST program

--- src/main.c
+++ src/main.c
@@ -1,12 +1,26 @@
 #include "buffer.h"
+#include "scan.h"
 #include "parser.h"
 
+
+Buffer buf;
+Scan scan;
+
+
+void init(int argc, char ** argv)
+{
+    initBuffer(&buf);
+    openProgramFile(&buf, argc, argv);
+
+    initScan(&scan, &buf);
+}
 
 
 int main(int argc, char ** argv)
 {
-    getProgram(argc, argv);
-    getProgramAST();
+    init(argc, argv);
 
+    getProgramAST(&scan);
+    
     return 0;
 }
